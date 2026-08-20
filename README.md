18424c55712d02427f5286ff4cd215008f92b196
zephyrbash <279035030+zephyrbash@users.noreply.github.com>
2026-08-20 13:39:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# GHubLetMeSleep
+
+A small Windows program I wrote in C to terminate the GHub agent process that constantly kept hanging a few times I put my system to sleep.
+
+## Usage
+
+Download the latest `GHubLetMeSleep` binary from the releases tab and put it somewhere on your system. You can pin it if needed to your taskbar or start menu, or, if you're like me, your stream deck.
+
+## Building
+
+This was built with C in CLion and MSVC
---
+++ main.c
@@ -0,0 +1,67 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <powrprof.h>
+#include <stdio.h>
+#include <string.h>
+
+static int kill_process() {
+    // Get a snapshot of all processes
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (snapshot == INVALID_HANDLE_VALUE) {
+        return 0;
+    }
+
+    // Create an entry of all processes
+    PROCESSENTRY32 processEntry;
+    processEntry.dwSize = sizeof(PROCESSENTRY32);
+
+    // If there are no processes, just return
+    if (!Process32First(snapshot, &processEntry)) {
+        CloseHandle(snapshot);
+        return 0;
+    }
+
+    int killed = 0;
+
+    do {
+        // const char *process_name = "lghub_agent.exe";
+        const char *process_name = "WindowsTerminal.exe";
+        if (_stricmp(processEntry.szExeFile, process_name) == 0) {
+            HANDLE process = OpenProcess(
+                PROCESS_TERMINATE,
+                FALSE,
+                processEntry.th32ProcessID
+                );
+
+            if (process) {
+                if (TerminateProcess(process, 0)) {
+                    printf("Killed %s (PID %lu)\n",
+                        processEntry.szExeFile,
+                        processEntry.th32ProcessID);
+
+                    killed = 1;
+                }
+
+                CloseHandle(process);
+            }
+        }
+    } while (Process32Next(snapshot, &processEntry));
+
+    CloseHandle(snapshot);
+    return killed;
+}
+
+int main(void) {
+    kill_process();
+
+    Sleep(1000);
+
+    // Put the system to sleep
+    if (!SetSuspendState(FALSE, FALSE, FALSE)) {
+        fprintf(stderr, "Failed to put the system to sleep\n");
+        return 1;
+    }
+
+    return 0;
+}
